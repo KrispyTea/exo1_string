055512ced99946dc360bdfc32e5edb1b65547491
KrispyTea <80962605+KrispyTea@users.noreply.github.com>
2021-04-15 17:56:02 +0200

    over

---
+++ main.js
@@ -0,0 +1,13 @@
+let prenom = prompt("Quel est ton prénom?");
+alert("Bonjour " + prenom)
+
+let age = prompt("Quel est ton age?")
+alert(`Tu as ${age} ans`)
+
+let donneprenom = prompt("Quel est ton prénom?")
+let donnenom = prompt("Quel est ton nom?")
+let nom = donnenom + " " + donneprenom
+alert(`Je m'appelle ${nom}`)
+
+let x = prompt("Quel est ton prénom?").toUpperCase + " " + prompt("Quel est ton nom?")
+alert(`Bonjour ${x}`)
